09580484d2a8b95157af1edcb6c0fb7944ffb75d
Brettink <brett_wad_12@hotmail.com>
2020-01-31 21:25:57 -0600

    Prefabs and Some scripts

---
+++ Assets/Scripts/Aster_Creat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aster_Creat : MonoBehaviour
+{
+    public PolygonCollider2D pl;
+    public float max_deviation = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int _num_points = Random.Range(5, 15);
+        Vector2[] points = new Vector2[_num_points];
+        for (int i = 0; i < _num_points; i++)
+        {
+            float x = Random.Range(-max_deviation, max_deviation)/10;
+            float y = Random.Range(-max_deviation, max_deviation)/10;
+            print(x + " " + y);
+            points[i] = new Vector2(x, y);
+        }
+        pl.points = points;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GMan.cs
+++ Assets/Scripts/GMan.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class GMan : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Sprite[] _aster_imgs;
+    public GameObject _aster_imgs_pref;
     public static int score = 0;
     public static float damage = 0f;
+    public static int difficulty = 1;
+    public int halfW = 50;
+    public int backDist = 50;
     void Start()
     {
         
@@ -16,6 +19,11 @@ public class GMan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < (difficulty * Mathf.Pow(Mathf.PI, difficulty)); i++){
+            int L_R = Random.Range(-halfW, halfW);
+            GameObject aster = Instantiate(_aster_imgs_pref, Vector2.zero, Quaternion.identity);
+            Vector2 pos = new Vector2(L_R, backDist);
+            aster.transform.position = pos;
+        }
     }
 }
