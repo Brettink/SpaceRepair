74bc23c3dc51cc64ef6da4d5dbc5b0e29558c9df
Brettink <brett_wad_12@hotmail.com>
2020-01-31 19:23:30 -0600

    More setup

---
+++ Assets/Scripts/GMan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite[] _aster_imgs;
+    public static int score = 0;
+    public static float damage = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
