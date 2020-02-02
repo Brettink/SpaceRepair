b8c3b4cd7b36698ad9807a948f465bed4c8745cb
Brettink <brett_wad_12@hotmail.com>
2020-02-02 13:40:26 -0600

    Some stuff

---
+++ Assets/Scripts/MG3.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MG3 : MonoBehaviour
+{
+
+    public GameObject panel;
+    // Start is called before the first frame update
+    public Text lineup;
+    string[] cons = new string[4];
+    void Awake()
+    {
+        List<string> aps = new List<string>();
+        aps.Add("a"); aps.Add("b"); aps.Add("c"); aps.Add("d");
+        for (int i = 0; i < 4; i++)
+        {
+            int ranI = Random.Range(0, aps.Count);
+            cons[i] = aps.ToArray()[ranI];
+            aps.RemoveAt(ranI);
+        }
+        
+    }
+
+    public void butClick(string name)
+    {
+        print("HI");
+    }
+
+    public void butClickN(int num)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
