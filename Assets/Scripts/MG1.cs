0bf2e521a211a1121cb4a7a14c5f469afdbd15f6
Brettink <brett_wad_12@hotmail.com>
2020-02-01 22:53:55 -0600

    Upadting Scripts

---
+++ Assets/Scripts/MG1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG1 : MonoBehaviour
+{
+    public Transform hammer, nails;
+    public Transform[] nailArr;
+    public SpriteRenderer hand;
+    float angleRest = 45f, angleHam = 90f;
+    bool hammers = true;
+    bool hamDir = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !hammers)
+        {
+            hammers = true;
+            hamDir = false;
+        }
+        if (hammers)
+        {
+            int dir = (hamDir)?-1: 1;
+            hammer.RotateAround(hammer.position, Vector3.forward, 3f*dir);
+            if (Mathf.Abs(hammer.localRotation.eulerAngles.z - ((!hamDir)?angleHam:angleRest)) < 1f)
+            {
+                if (hamDir)
+                {
+                    hammers = false;
+                }
+                hamDir = true;
+            }
+        }
+        Vector2 pos = nails.localPosition;
+        pos.x += .02f;
+        if (pos.x > 6)
+        {
+            pos.x = -10;
+        }
+        nails.localPosition = pos;
+    }
+}
---
+++ Assets/Scripts/nailsHit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nailsHit : MonoBehaviour
+{
+    public static bool hit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hit = true;
+        transform.parent = null;
+        transform.position.Set(0, -400, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
